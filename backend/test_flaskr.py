e4a9cb5c071e88733792615f5b09638f4c03a130
Kirankumar Sripati <krnsripati@gmail.com>
2020-05-14 13:59:16 +0530

    Add test cases for POST /questions endpoint

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -37,6 +37,7 @@ class TriviaTestCase(unittest.TestCase):
     """
 
     def test_get_categories(self):
+        '''Get list of categories'''
         res = self.client().get('/categories')
         data = json.loads(res.data)
 
@@ -45,6 +46,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(len(data['categories']))
 
     def test_get_questions(self):
+        '''Get list of questions'''
         res = self.client().get('/questions')
         data = json.loads(res.data)
 
@@ -55,6 +57,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['total_questions'])
 
     def test_404_get_questions(self):
+        '''Show error if invalid page number is provided'''
         res = self.client().get('/questions?page=9999999')
         data = json.loads(res.data)
 
@@ -62,6 +65,106 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
         self.assertEqual(data['error'], 404)
         self.assertEqual(data['message'], 'questions not found')
+
+    def missing_field_create_question(self, question, missing):
+        '''Try to create a new question with a missing field'''
+        missing_field_question = {}
+
+        for key in question:
+            if key != missing:
+                missing_field_question[key] = question[key]
+
+        res = self.client().post('/questions', json=missing_field_question)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+        self.assertTrue(data['message'], '{} cannot be blank'.format(missing))
+
+    def test_create_question(self):
+        '''Can create new question'''
+        new_question = {
+            'question': 'In which city is Jiddu Krishnamurti Foundation \
+                School in Maharashtra?',
+            'answer': 'Pune',
+            'category': 3,
+            'difficulty': 3
+        }
+
+        res = self.client().post('/questions', json=new_question)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['created'])
+
+    def test_error_create_question(self):
+        '''If any of the field is missing, throw error'''
+        new_question = {
+            'question': 'In which city is Jiddu Krishnamurti Foundation \
+                School in Maharashtra?',
+            'answer': 'Pune',
+            'category': 3,
+            'difficulty': 3
+        }
+
+        # case 1, question missing
+        self.missing_field_create_question(
+            question=new_question, missing='question')
+
+        # case 2, answer missing
+        self.missing_field_create_question(
+            question=new_question, missing='answer')
+
+        # case 3, category missing
+        self.missing_field_create_question(
+            question=new_question, missing='answer')
+
+        # case 4, difficulty missing
+        self.missing_field_create_question(
+            question=new_question, missing='difficulty')
+
+    def test_search_question(self):
+        '''Search for question'''
+        search_json = {
+            'searchTerm': 'movie'
+        }
+        res = self.client().post('/questions', json=search_json)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['total_questions'])
+
+    def test_404_search_question_page(self):
+        '''Fail to search for question with invalid page'''
+        search_json = {
+            'searchTerm': 'a',
+            'page': 99999
+        }
+        res = self.client().post('/questions', json=search_json)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(
+            data['message'], 'no questions found with term - {}'.format(
+                search_json['searchTerm']))
+
+    def test_404_search_question(self):
+        '''Fail to search for question with non existign term'''
+        search_json = {
+            'searchTerm': '~!@#$%^&*()_+'
+        }
+        res = self.client().post('/questions', json=search_json)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(
+            data['message'], 'no questions found with term - {}'.format(
+                search_json['searchTerm']))
 
 
 # Make the tests conveniently executable
